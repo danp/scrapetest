ed364bc5186836bec0dfee90538c0bbc3d1bbb74
Dan Peterson <danp@danp.net>
2020-11-19 09:00:18 -0400

    bin: capture report_servicearea.json

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 dir="$(curl --silent --fail http://outagemap.nspower.ca/resources/data/external/interval_generation_data/metadata.json | jq -r .directory)"
+durl="http://outagemap.nspower.ca/resources/data/external/interval_generation_data/$dir"
 
-for k in 030231 030233 030320 030321 030322 030323; do
-    if curl --silent --fail --retry 3 --max-time 15 -o "$k.json" "http://outagemap.nspower.ca/resources/data/external/interval_generation_data/$dir/outages/$k.json"; then
+for k in 030231 030233 030320 030321 030322 030323 ; do
+    if curl --silent --fail --retry 3 --max-time 15 -o "$k.json" "$durl/outages/$k.json"; then
 	jq -S . "$k.json" > "$k.json.tmp"
 	mv "$k.json.tmp" "$k.json"
     fi
+done
+
+for f in report_servicearea; do
+    curl --silent --fail --retry 3 --max-time 15 -O "$durl/$f.json"
+    jq -S . "$f.json" > "$f.json.tmp"
+    mv "$f.json.tmp" "$f.json"
 done
